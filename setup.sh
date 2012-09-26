929fab463d77272d59cb2171c5f8cf9ba34d813c
Paul Bienkowski <opatutlol@aol.com>
2012-09-26 16:21:48 +0200

    Added python-dateutil to dependency list

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 virtualenv -p python2 env
 . env/bin/activate
-pip install --upgrade flask flask-sqlalchemy flask-wtf flask-markdown iso8601 pytz pygments
+pip install --upgrade flask flask-sqlalchemy flask-wtf flask-markdown iso8601 pytz pygments python-dateutil
 # flask-mail flask-login python-dateutil requests
